952f617f0629bd10809e63fcb6225548a4aea63d
Aman <amankaziak1999@gmail.com>
2025-07-26 21:34:34 +0530

    added folder structure changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Explore from './pages/Explore'
+import LandingPage from './pages/LandingPage'
 
 const App = () => {
   return (
-    <div className='bg-red-500'>App</div>
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/landing-page" element={<LandingPage />} />
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.tsx'
 import './assets/global.css'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
